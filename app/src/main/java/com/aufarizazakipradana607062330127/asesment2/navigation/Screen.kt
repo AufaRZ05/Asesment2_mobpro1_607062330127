da3b84b8827fea168a2fe861e2d28d02dbc67701
Aufa Riza Zaki Pradana <aufa.rzp10@gmail.com>
2025-05-04 17:43:47 +0700

    Membuat tampilan form kelola produk

--- app/src/main/java/com/aufarizazakipradana607062330127/asesment2/navigation/Screen.kt
+++ app/src/main/java/com/aufarizazakipradana607062330127/asesment2/navigation/Screen.kt
@@ -2,4 +2,5 @@ package com.aufarizazakipradana607062330127.asesment2.navigation
 
 sealed class Screen(val route: String) {
     data object Home: Screen("mainScreen")
+    data object FormBaru: Screen("detailScreen")
 }
---
+++ app/src/main/java/com/aufarizazakipradana607062330127/asesment2/ui/screen/DetailScreen.kt
@@ -0,0 +1,172 @@
+package com.aufarizazakipradana607062330127.asesment2.ui.screen
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.aufarizazakipradana607062330127.asesment2.R
+import com.aufarizazakipradana607062330127.asesment2.ui.theme.Asesment2Theme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen() {
+    var namaMerek by remember { mutableStateOf("") }
+    var harga by remember { mutableStateOf("") }
+    var stok by remember { mutableStateOf("") }
+    var kategori by remember { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.tambah_produk))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { padding ->
+        FormProduk(
+            brandName = namaMerek,
+            onBrandNameChange = { namaMerek = it },
+            price = harga,
+            onPriceChange = { harga = it },
+            stock = stok,
+            onStockChange = { stok = it},
+            category = kategori,
+            onCategoryChange = { kategori = it },
+            modifier = Modifier.padding(padding)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun DetailScreenPreview() {
+    Asesment2Theme {
+        DetailScreen()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormProduk(
+    brandName: String, onBrandNameChange: (String) -> Unit,
+    price: String, onPriceChange: (String) -> Unit,
+    stock: String, onStockChange: (String) -> Unit,
+    category: String, onCategoryChange: (String) -> Unit,
+    modifier: Modifier
+) {
+    val kategoriList = listOf(
+        stringResource(R.string.lampu),
+        stringResource(R.string.kipas_angin),
+        stringResource(R.string.speaker)
+    )
+    var expanded by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        OutlinedTextField(
+            value = brandName,
+            onValueChange = { onBrandNameChange(it) },
+            label = { Text(text = stringResource(R.string.nama_merek)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = price,
+            onValueChange = {
+                if (it.all { char -> char.isDigit() }) {
+                    onPriceChange(it)
+                }
+            },
+            label = { Text(text = stringResource(R.string.harga)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = stock,
+            onValueChange = {
+                if (it.all { char -> char.isDigit() }) {
+                    onStockChange(it)
+                }
+            },
+            label = { Text(text = stringResource(R.string.stok)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }
+        ) {
+            OutlinedTextField(
+                value = category,
+                onValueChange = {},
+                readOnly = true,
+                label = { Text(stringResource(R.string.kategori)) },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                modifier = Modifier
+                    .menuAnchor()
+                    .fillMaxWidth()
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                kategoriList.forEach { selectedOption ->
+                    DropdownMenuItem(
+                        text = { Text(selectedOption) },
+                        onClick = {
+                            onCategoryChange(selectedOption)
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
